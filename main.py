d488b9d175d4958df4c5838c371577d4a5bf9bd8
tes-kious <88243937+tes-kious@users.noreply.github.com>
2023-06-25 11:41:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
+import mongoservices
 
 app = Flask(__name__)
-
+mongosvc = mongoservices.mongoservices()
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"Hallo": "Welcome to your Flask Application"})
+
+@app.route('/api/menu')
+def menu():
+    return jsonify({"code": "1", "status": "OK", "menus":"menus"})
+
+@app.route('/api/samples', methods=['POST'])
+def save_sample():
+    data = request.json
+    return mongosvc.save_sample(data)	
 
 
 if __name__ == '__main__':
