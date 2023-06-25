1384e679cbf8f75c31d54090f811b7488a07cb9d
tes-kious <88243937+tes-kious@users.noreply.github.com>
2023-06-25 17:44:50 +0700

    Update mongoservices.py

--- mongoservices.py
+++ mongoservices.py
@@ -27,9 +27,7 @@ class mongoservices:
             #return jsonify(data)
             db = mongoservices.condb(self)
             samples_coll = db.AllRideRequests
-            sort = { "_id": -1 };
-            limit = 1;
-            cursor = samples_coll.find(data, {"_id": 0 }).sort(sort).limit(limit);
+            cursor = samples_coll.find(data, {"_id": 0 }).sort("_id": -1).limit(1);
             #cursor = samples_coll.find_one(data, {"_id": 0 })
             #json_data = json.dumps(cursor)
             #return jsonify(json_data)  
